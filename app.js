1c1cbb1efd1baefb9f5f3055b0d682cd69910048
san <2004da@gmail.com>
2025-01-13 08:40:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 });
 
 // 监听端口
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
